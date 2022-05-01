6686892b000e647985fa3729d14c2c8b635a9699
Yasser Emam <YasserEmam2002@gmail.com>
2022-05-01 21:28:56 +0200

    finished put bug

--- index.js
+++ index.js
@@ -26,23 +26,35 @@ app.get('/api/courses/:id', (req, res) => {
     else res.status(404).send("The course does not exist")
 })
 
-app.post('/api/courses', (req, res) => {
+app.post('/api/courses', async function (req, res)  {
     // input validation
-    if (!req.body.name) { 
-        res.status(400).send('Name cant be empty.');
-        return;
-    } else if (req.body.name.length < 3) {
-        res.status(400).send('Name cant be less than 3 characters');
-        return;
-    }
 
-    const course = {
-        id: courses.length+1,
-        name: req.body.name
-    };
-    courses.push(course);
+    // if (!req.body.name) { 
+    //     res.status(400).send('Name cant be empty.');
+    //     return;
+    // } else if (req.body.name.length < 3) {
+    //     res.status(400).send('Name cant be less than 3 characters');
+    //     return;
+    // }
 
-    res.send(course);
+    const schema = Joi.object({
+        name: Joi.string().min(3).required(),
+    });
+
+    try {
+        const value = await schema.validateAsync(req.body);
+        res.status(200).send(value);
+
+        const course = {
+            id: courses.length+1,
+            name: req.body.name
+        };
+        courses.push(course);
+    } catch (err) {
+        console.log("error");
+        res.status(400).send(err);
+    } 
+
 });
 
 
